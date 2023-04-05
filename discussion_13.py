e2ff79f20e4c23a76a85755e8fb1715f86118224
yuyuuyang <112489804+yuyuuyang@users.noreply.github.com>
2023-04-05 13:46:16 -0400

    Add files via upload

---
+++ discussion_13.py
@@ -0,0 +1,102 @@
+import unittest
+import sqlite3
+import json
+import os
+import matplotlib.pyplot as plt
+# starter code
+
+# Create Database
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
+
+
+# TASK 1
+# CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
+def create_employee_table(cur, conn):
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS employees (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            age INTEGER,
+            position TEXT,
+            department TEXT,
+            salary REAL
+        )
+    ''')
+    conn.commit()
+
+# ADD EMPLOYEE'S INFORMTION TO THE TABLE
+
+def add_employee(filename, cur, conn):
+    #load .json file and read job data
+    # WE GAVE YOU THIS TO READ IN DATA
+    f = open(os.path.abspath(os.path.join(os.path.dirname(__file__), filename)))
+    file_data = f.read()
+    f.close()
+    # THE REST IS UP TO YOU
+    pass
+
+# TASK 2: GET JOB AND HIRE_DATE INFORMATION
+def job_and_hire_date(cur, conn):
+    cur.execute('''
+        SELECT job_title, hire_date
+        FROM employees
+        JOIN jobs ON employees.job_id = jobs.job_id
+        ORDER BY hire_date ASC
+    ''')
+    rows = cur.fetchall()
+    earliest_employee = rows[0]
+    return earliest_employee[0]
+
+
+
+# TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
+# Apply JOIN clause to match individual employees
+def problematic_salary(cur, conn):
+    pass
+
+# TASK 4: VISUALIZATION
+def visualization_salary_data(cur, conn):
+    pass
+
+class TestDiscussion12(unittest.TestCase):
+    def setUp(self) -> None:
+        self.cur, self.conn = setUpDatabase('HR.db')
+
+    def test_create_employee_table(self):
+        self.cur.execute("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='employees'")
+        table_check = self.cur.fetchall()[0][0]
+        self.assertEqual(table_check, 1, "Error: 'employees' table was not found")
+        self.cur.execute("SELECT * FROM employees")
+        count = len(self.cur.fetchall())
+        self.assertEqual(count, 13)
+
+    def test_job_and_hire_date(self):
+        self.assertEqual('President', job_and_hire_date(self.cur, self.conn))
+
+    def test_problematic_salary(self):
+        sal_list = problematic_salary(self.cur, self.conn)
+        self.assertIsInstance(sal_list, list)
+        self.assertEqual(sal_list[0], ('Valli', 'Pataballa'))
+        self.assertEqual(len(sal_list), 4)
+
+
+def main():
+    # SETUP DATABASE AND TABLE
+    cur, conn = setUpDatabase('HR.db')
+    create_employee_table(cur, conn)
+
+    add_employee("employee.json",cur, conn)
+
+    job_and_hire_date(cur, conn)
+
+    wrong_salary = (problematic_salary(cur, conn))
+    print(wrong_salary)
+
+if __name__ == "__main__":
+    main()
+    unittest.main(verbosity=2)
+
